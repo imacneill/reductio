f3b3d37bbab89fb18631802dd4614bafe4425282
ian@posiba.com <ian@posiba.com>
2016-01-15 11:19:24 -0800

    added a 2nd alias to the example in the test suite and then added a 2nd test. hopefully this will help catch bugs due to using multiple aliases at once.

--- test/alias.spec.js
+++ test/alias.spec.js
@@ -1,6 +1,7 @@
 // Alias tests
 describe('Alias function', function () {
     var group;
+    var values = {};
 
     beforeEach(function () {
         var data = crossfilter([
@@ -17,23 +18,33 @@ describe('Alias function', function () {
 
         var reducer = reductio()
                 .count(true)
-                .alias({ newCount: function(g) { return g.count; } });
+                .alias({ newCount: function(g) { return g.count; },
+                         theCount: function(g) { return g.count+' items... AH AH AH'; }
+                      });
 
         reducer(group);
+
+
+        group.top(Infinity).forEach(function (d) {
+            values[d.key] = d.value;
+        });
     });
 
     it('has three groups', function () {
         expect(group.top(Infinity).length).toEqual(3);
     });
 
-    it('grouping have the right counts', function () {
-        var values = {};
-        group.top(Infinity).forEach(function (d) {
-            values[d.key] = d.value;
-        });
+    it('grouping for first alias have the right counts', function () {
 
         expect(values['one'].newCount()).toEqual(3);
         expect(values['two'].newCount()).toEqual(2);
         expect(values['three'].newCount()).toEqual(1);
+    });
+
+    it('groupings for second alias have the right values', function(){
+
+        expect(values['one'].theCount()).toMatch('3 items... AH AH AH');
+        expect(values['two'].theCount()).toMatch('2 items... AH AH AH');
+        expect(values['three'].theCount()).toMatch('1 items... AH AH AH');
     });
 });
